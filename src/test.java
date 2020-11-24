38bcd442e55b575cd7180f953f41f938c5602aec
Mehmet Ali Gencay <gencayali20@gmail.com>
2020-11-24 20:34:14 +0100

    ss

--- src/test.java
+++ src/test.java
@@ -2,4 +2,9 @@ public class test {
     public static void main(String[] args) {
         System.out.println("1.islem");
     }
+
+    public static void yeniMethod(){
+        System.out.println("yeni method hazir");
+        System.out.println("sepete ekle kismlari buralar");
+    }
 }
